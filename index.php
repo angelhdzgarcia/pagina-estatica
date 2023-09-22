dabb1807420e32b488124355461a0c874364f311
angelhdzgarcia <126221760+angelhdzgarcia@users.noreply.github.com>
2023-09-22 14:39:33 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Despliegue de Aplicaciones Web</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #007bff;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            font-size: 36px;
+        }
+
+        main {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+        }
+
+        p {
+            font-size: 18px;
+            line-height: 1.6;
+        }
+
+        ul {
+            list-style-type: disc;
+            padding-left: 20px;
+        }
+
+        footer {
+            background-color: #007bff;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Despliegue de Aplicaciones Web</h1>
+    </header>
+    
+    <main>
+        <h2>¿Qué es el despliegue de aplicaciones web?</h2>
+        <p>El despliegue de aplicaciones web es el proceso de poner una aplicación web en funcionamiento y accesible para los usuarios finales. Implica tomar el código de la aplicación que ha sido desarrollado y probado localmente y llevarlo a un servidor o plataforma en línea donde otros puedan acceder a ella.</p>
+
+        <h2>Principales pasos en el despliegue de aplicaciones web:</h2>
+        <ul>
+            <li>Configuración de un servidor web.</li>
+            <li>Configuración de una base de datos si es necesario.</li>
+            <li>Carga de archivos y código de la aplicación al servidor.</li>
+            <li>Configuración de nombres de dominio y DNS.</li>
+            <li>Pruebas en un entorno de producción.</li>
+            <li>Puesta en marcha para usuarios finales.</li>
+        </ul>
+
+        <h2>Consideraciones importantes:</h2>
+        <p>El despliegue de aplicaciones web debe realizarse cuidadosamente para garantizar la disponibilidad, seguridad y rendimiento de la aplicación. Es importante realizar pruebas exhaustivas antes de poner la aplicación en producción y mantener un proceso de despliegue continuo para realizar actualizaciones y mejoras de manera regular.</p>
+    </main>
+
+    <footer>
+        &copy; 2023 Despliegue de Aplicaciones Web
+    </footer>
+</body>
+</html>
